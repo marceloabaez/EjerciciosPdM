90be88f20d2608a93e5693c8b8624038b06f7ec9
marcelo <marceloanibalbaez@gmail.com>
2023-04-21 12:00:05 -0300

    Final 1.2

--- TP_Integrador_RTC/Drivers/API/src/API_RTC_port.c
+++ TP_Integrador_RTC/Drivers/API/src/API_RTC_port.c
@@ -5,16 +5,16 @@
 #include"stm32f4xx.h"
 #include"stm32f4xx_hal_i2c.h"
 
-#define I2C_TO 1000 // Timeout de la comunicación I2C
+#define I2C_TO 80 // Timeout de la comunicación I2C
 
 extern I2C_HandleTypeDef hi2c1;
 extern uint8_t buff[20];
 
 
 void I2C_Read(uint16_t i2c_add, uint16_t mem_add, uint16_t size){
-HAL_I2C_Mem_Read(&hi2c1, i2c_add, mem_add, 1, buff, 3,80);
+HAL_I2C_Mem_Read(&hi2c1, i2c_add, mem_add, 1, buff, 3, I2C_TO);
 }
 
 void I2C_Write(uint16_t i2c_add, uint16_t mem_add, uint16_t size){
-HAL_I2C_Mem_Write(&hi2c1, i2c_add, mem_add, 1, buff, 3, 80);
+HAL_I2C_Mem_Write(&hi2c1, i2c_add, mem_add, 1, buff, 3, I2C_TO);
 }
